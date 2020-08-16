309e238e8b91406670b9a0d50831469bb27887d7
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2020-08-16 23:56:32 +0400

    Create crw_server.cpp

---
+++ advanced_crow/crw_server.cpp
@@ -0,0 +1,89 @@
+#include "crow.h"
+#include <iostream>
+using namespace std;
+
+
+// Just Function for generating random String
+string generateRandomString(size_t length)
+{
+  const char* charmap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const size_t charmapLength = strlen(charmap);
+  auto generator = [&](){ return charmap[rand()%charmapLength]; };
+  string result;
+  result.reserve(length);
+  generate_n(back_inserter(result), length, generator);
+  return result;
+}
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/ex_json")
+    ([](const crow::request& req) {
+        // Print all parameters
+    std::cout << "Params:" << req.url_params << "\n\n";
+    // Or you can print specific parameters
+        //std::cout << req.url_params.get("foo") << "\n\n";
+        // std::cout << req.url_params.get("pew") << "\n\n";
+
+        // Get List from parameters
+        auto array_strings = req.url_params.get_list("count");
+
+        // Lenght of array
+        int c = array_strings.size();
+        // Convert array of Str to array of Int
+        //int arrayOfIntegers[c];
+        //for(int i = 0; i < c; i++) arrayOfIntegers[i] = std::stoi(array_strings[i]);
+
+        // You can print all values in Array
+        /*
+        for(const auto& countVal : count) {
+                std::cout << " - " << countVal << '\n';
+        }
+        */
+
+        // Return Json
+        crow::json::wvalue x;
+        x["success"] = 1;
+        x["message"] = "Hello, World!";
+        x["foo"] = req.url_params.get("foo");
+        x["pew_str"] = req.url_params.get("pew");
+        x["double_param"] = atof(req.url_params.get("double_param") )+ 1;
+        // get integer:
+        x["pew_int_stoi"] = 3 + std::stoi(req.url_params.get("pew"));
+        // get double:
+        x["pew_double"] = atof(req.url_params.get("pew")) / 2;
+        x["list_str"] = array_strings;
+
+    // Return array
+    x["arr1"][0] = 1;
+    x["arr1"][1] = 3;
+    x["arr1"][2] = 4;
+
+    // You can even declare new parameter and assign not 0 element but 1
+    x["arr2"][1] = 2;
+
+        return crow::response(x);
+    });
+
+    CROW_ROUTE(app, "/ex_json_arr")([]() {
+    crow::json::wvalue x;
+    std::vector<int> ids = {1,2,3};
+    std::vector<string> names = {"test1", "test2", "test3"};
+    size_t i;
+
+    for (i = 0; i < ids.size(); ++i) {
+         x["arr_int"][i] = ids.at(i);
+         // x[i]["name"] = names.at(i);
+    }
+    for (i = 0; i < ids.size(); ++i) {
+         x["arr_str"][i] = generateRandomString(4);
+         // x[i]["name"] = names.at(i);
+    }
+    x["name"] = "ABC";
+    return crow::response(x);
+});
+
+    app.port(18080).run();
+}
